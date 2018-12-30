7bb8c6290ec22435dd96872bef469ae9856def10
SAEED MEHRANG <saeedmehrang@users.noreply.github.com>
2018-12-30 22:01:04 +0200

    Update canny_edge_detection_16.py

--- canny_edge_detection_16/canny_edge_detection_16.py
+++ canny_edge_detection_16/canny_edge_detection_16.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("tomatoes.jpg",1)
+img = cv2.imread("images_to_test_scripts/tomatoes.jpg",1)
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 res,thresh = cv2.threshold(hsv[:,:,0], 25, 255, cv2.THRESH_BINARY_INV)
