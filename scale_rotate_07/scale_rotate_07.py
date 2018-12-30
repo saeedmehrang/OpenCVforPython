4b30e36f9d03f8e785bf324db963f2b94290812a
SAEED MEHRANG <saeedmehrang@users.noreply.github.com>
2018-12-30 22:14:35 +0200

    Update scale_rotate_07.py

--- scale_rotate_07/scale_rotate_07.py
+++ scale_rotate_07/scale_rotate_07.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("players.jpg",1)
+img = cv2.imread("images_to_test_scripts/players.jpg",1)
 
 # Scale
 img_half = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
